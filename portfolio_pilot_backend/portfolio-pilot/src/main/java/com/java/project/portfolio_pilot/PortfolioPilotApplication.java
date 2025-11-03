89a08ea0dd9f751b1efc70dc72fbbb6bbf2a8ea5
Panait Alexandru <aleexip81@gmail.com>
2025-11-03 18:25:47 +0200

    Setup

---
+++ portfolio_pilot_backend/portfolio-pilot/src/main/java/com/java/project/portfolio_pilot/PortfolioPilotApplication.java
@@ -0,0 +1,13 @@
+package com.java.project.portfolio_pilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioPilotApplication.class, args);
+	}
+
+}
---
+++ portfolio_pilot_backend/portfolio-pilot/src/test/java/com/java/project/portfolio_pilot/PortfolioPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.project.portfolio_pilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
